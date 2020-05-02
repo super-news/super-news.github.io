6b04b98bccd860702aade6a30050773295c7ccff
Ilya_Tyurin <ti@b2b-labs.ru>
2020-05-02 17:31:22 +0300

    2

---
+++ js/feedback.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+    $('.feedback').click(function() {
+        $('.popup-window').show();
+    });
+    $('.close-popup').click(function() {
+        $('.popup-window').hide();
+    });
+
+});
